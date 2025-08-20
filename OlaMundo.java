ab6bb29d4dd5d90a15b9a563b8af6268e506c176
gwedosun <gabriellabraga7@gmail.com>
2025-08-20 11:45:18 -0300

    primeiro commit

---
+++ OlaMundo.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+class olaMundo {
+
+	public static void main(String[] args){
+		System.out.println("Olá, mundo!");
+		JOptionPane.showMessageDialog(null, "Ola, mundo!");
+		System.exit(0);
+	}
+
+}
